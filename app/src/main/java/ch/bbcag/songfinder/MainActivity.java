8441f182e5f3f3871c7cb4b6e4b589cdb837ac51
Monigadon Céline <bmonic@bbc-be.local>
2018-06-12 11:25:26 +0200

    homescreen wip
    wip

--- app/src/main/java/ch/bbcag/songfinder/MainActivity.java
+++ app/src/main/java/ch/bbcag/songfinder/MainActivity.java
@@ -1,13 +1,21 @@
 package ch.bbcag.songfinder;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActionBar actionBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        actionBar = getSupportActionBar();
+        actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#32353A")));
     }
+
 }

a223ce19ea1d06e9d7aa4a24ddd07d070519afa5
HavaFuga <hava_fuga@outlook.com>
2018-06-18 16:19:37 +0200

    Icon in actionBar with onClick

--- app/src/main/java/ch/bbcag/songfinder/Favorits.java
+++ app/src/main/java/ch/bbcag/songfinder/Favorits.java
@@ -1,10 +1,14 @@
 package ch.bbcag.songfinder;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Favorits extends AppCompatActivity {
 
@@ -16,6 +20,26 @@ public class Favorits extends AppCompatActivity {
         actionBar = getSupportActionBar();
         actionBar.setTitle("Favoriten");
         actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#32353A")));
+
+        //Icon in ActionBar
+        actionBar.setDisplayOptions(actionBar.getDisplayOptions()
+                | ActionBar.DISPLAY_SHOW_CUSTOM);
+        ImageView imageView = new ImageView(actionBar.getThemedContext());
+        imageView.setScaleType(ImageView.ScaleType.CENTER);
+        imageView.setImageResource(R.drawable.heart_red);
+        ActionBar.LayoutParams layoutParams = new ActionBar.LayoutParams(
+                ActionBar.LayoutParams.WRAP_CONTENT,
+                ActionBar.LayoutParams.WRAP_CONTENT, Gravity.RIGHT
+                | Gravity.CENTER_VERTICAL);
+        layoutParams.rightMargin = 40;
+        imageView.setLayoutParams(layoutParams);
+        actionBar.setCustomView(imageView);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Favorits.this, Favorits.class));
+            }
+        });
     }
 
 

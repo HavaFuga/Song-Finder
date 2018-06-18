9c547a775aa0916f7e1dd2bea123f0139b5baaa6
HavaFuga <hava_fuga@outlook.com>
2018-06-18 12:09:18 +0200

    Revert "fav"
    This reverts commit cfd18539ce77e3c14f79df4d9e1129715768dfb8.

--- app/src/main/java/ch/bbcag/songfinder/MainActivity.java
+++ app/src/main/java/ch/bbcag/songfinder/MainActivity.java
@@ -1,18 +1,14 @@
 package ch.bbcag.songfinder;
 
-import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     ActionBar actionBar;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,13 +18,7 @@ public class MainActivity extends AppCompatActivity {
         actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#32353A")));
         //actionBar.setIcon(@drawable/herz_weiss.svg);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
-        // getSupportActionBar().setIcon(res.drawable.herz_weiss.svg);
-        Button b = (Button) findViewById(R.id.btnSearch);
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, list.class));
-            }
-        });
+       // getSupportActionBar().setIcon(res.drawable.herz_weiss.svg);
     }
+
 }
--- app/src/main/java/ch/bbcag/songfinder/list.java
+++ app/src/main/java/ch/bbcag/songfinder/list.java
@@ -1,8 +1,5 @@
 package ch.bbcag.songfinder;
 
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -12,8 +9,5 @@ public class list extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
-        ActionBar aBlist = getSupportActionBar();
-        aBlist.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#32353A")));
-        aBlist.setTitle("Vorschläge");
     }
 }

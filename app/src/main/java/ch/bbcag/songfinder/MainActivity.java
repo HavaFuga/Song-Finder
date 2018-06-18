7c1703e4ba211fddcfae758d546a1d7b55773675
HavaFuga <hava_fuga@outlook.com>
2018-06-18 12:15:02 +0200

    merge

--- app/src/main/java/ch/bbcag/songfinder/MainActivity.java
+++ app/src/main/java/ch/bbcag/songfinder/MainActivity.java
@@ -1,14 +1,19 @@
 package ch.bbcag.songfinder;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     ActionBar actionBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,7 +23,20 @@ public class MainActivity extends AppCompatActivity {
         actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#32353A")));
         //actionBar.setIcon(@drawable/herz_weiss.svg);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
-       // getSupportActionBar().setIcon(res.drawable.herz_weiss.svg);
+        // getSupportActionBar().setIcon(res.drawable.herz_weiss.svg);
+        Button b = (Button) findViewById(R.id.btnSearch);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, list.class));
+            }
+        });
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fABtn);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Favorits.class));
+            }
+        });
     }
-
 }

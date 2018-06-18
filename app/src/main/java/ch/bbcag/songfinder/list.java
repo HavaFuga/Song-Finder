fa41a54db06b2a4cc4b6c178ba42ab191a43b9fd
Monigadon Céline <bmonic@bbc-be.local>
2018-06-18 11:22:07 +0200

    onclicklistener search -> list
    sd

--- app/src/main/java/ch/bbcag/songfinder/list.java
+++ app/src/main/java/ch/bbcag/songfinder/list.java
@@ -1,5 +1,8 @@
 package ch.bbcag.songfinder;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,8 @@ public class list extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+        ActionBar aBlist = getSupportActionBar();
+        aBlist.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#32353A")));
+        aBlist.setTitle("Vorschläge");
     }
 }

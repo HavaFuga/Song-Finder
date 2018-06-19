2dcd0e21a8b5ea7cf65e9ddc9950e98e21b7b77e
Fuga Hava <bfugah@bbc-be.local>
2018-06-19 16:08:35 +0200

    fav tracks

--- app/src/main/java/ch/bbcag/songfinder/model/FavoritTrack.java
+++ app/src/main/java/ch/bbcag/songfinder/model/FavoritTrack.java
@@ -1,41 +1,4 @@
-<<<<<<< HEAD
-///*package ch.bbcag.songfinder.model;
-//
-//import java.util.List;
-//
-//public class FavoritTrack {
-//    List<TrackObject> favorites = null;
-//    public void addTrackToFavorites(TrackObject track){
-//        favorites.add(track);
-//    }
-//
-//    public List<TrackObject> getFavorites() {
-//        return favorites;
-//    }
-//
-//    public void fillListView(List<TrackObject> favorites){
-//<<<<<<< HEAD
-//        ListView favorit_tracks = findViewById(R.id.favorit_tracks);
-//
-//        ArrayAdapter<TrackObject> listAdapter;
-//        listAdapter = new ArrayAdapter<FavoritTrack>(this, R.layout.activity_favorits, favorites);
-//=======
-//        /*
-//        ListView listView_ft = findfViewById(R.id.favorit_tracks);
-//
-//        ArrayAdapter<TrackObject> arrayAdapter = new ArrayAdapter<TrackObject>(
-//                this,
-//                android.R.layout.simple_list_item_1,
-//                trackObjects);
-//
-//        lvSearch.setAdapter(arrayAdapter);
-//>>>>>>> 8b58b5418e480aba9d111616962c90d83ea3e328
-//
-//        // Assign adapter to ListView
-//        favorit_tracks.setAdapter( listAdapter );*/
-//    }
-//}*/
-=======
+
 package ch.bbcag.songfinder.model;
 
 import java.util.List;
@@ -65,4 +28,3 @@ public class FavoritTrack {
         favorit_tracks.setAdapter( listAdapter );*/
     }
 }
->>>>>>> 1cc78ef8cf665fe4f0ff2615bcf69223288f91be

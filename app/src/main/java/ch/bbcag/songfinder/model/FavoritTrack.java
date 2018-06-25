21f29aa9f64d7865115616b71c2482c5af6f932b
HavaFuga <hava_fuga@outlook.com>
2018-06-25 12:47:29 +0200

    fav

--- app/src/main/java/ch/bbcag/songfinder/model/FavoritTrack.java
+++ app/src/main/java/ch/bbcag/songfinder/model/FavoritTrack.java
@@ -1,29 +1,33 @@
 package ch.bbcag.songfinder.model;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 import java.util.List;
 
-public class FavoritTrack {
-    List<TrackObject> favorites = null;
+import ch.bbcag.songfinder.R;
+
+public class FavoritTrack extends AppCompatActivity {
+    List<TrackObject> favorites = new ArrayList<TrackObject>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
     public void addTrackToFavorites(TrackObject track){
         favorites.add(track);
+        //Heart make red
     }
 
     public List<TrackObject> getFavorites() {
         return favorites;
     }
 
-    public void fillListView(List<TrackObject> favorites){
-        /*
-        ListView listView_ft = findfViewById(R.id.favorit_tracks);
-
-        ArrayAdapter<TrackObject> arrayAdapter = new ArrayAdapter<TrackObject>(
-                this,
-                android.R.layout.simple_list_item_1,
-                trackObjects);
-
-        lvSearch.setAdapter(arrayAdapter);
-
-        // Assign adapter to ListView
-        favorit_tracks.setAdapter( listAdapter );*/
-    }
 }

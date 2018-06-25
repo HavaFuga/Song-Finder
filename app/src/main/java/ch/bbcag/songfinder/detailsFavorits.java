a8b17615b4fd6aba44d0e415b78ab49ee89dfcdc
HavaFuga <hava_fuga@outlook.com>
2018-06-25 12:52:19 +0200

    detailsFavorit

--- app/src/main/java/ch/bbcag/songfinder/detailsFavorits.java
+++ app/src/main/java/ch/bbcag/songfinder/detailsFavorits.java
@@ -1,5 +1,7 @@
 package ch.bbcag.songfinder;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;

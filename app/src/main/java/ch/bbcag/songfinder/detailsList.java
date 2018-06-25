ecf2f719dd58ff50cd7618ef6c2232faf32be576
Monigadon Céline <bmonic@bbc-be.local>
2018-06-25 12:59:29 +0200

    listdetails

--- app/src/main/java/ch/bbcag/songfinder/detailsList.java
+++ app/src/main/java/ch/bbcag/songfinder/detailsList.java
@@ -8,6 +8,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import org.jmusixmatch.entity.lyrics.Lyrics;
+
 import ch.bbcag.songfinder.model.TrackObject;
 
 public class detailsList extends AppCompatActivity {
@@ -21,21 +23,33 @@ public class detailsList extends AppCompatActivity {
         setContentView(R.layout.activity_details_list);
 
         listView = (ListView) findViewById(R.id.lv_search);
-         adapter = (ArrayAdapter) listView.getAdapter();
+        adapter = (ArrayAdapter) listView.getAdapter();
 
         int trackId = getIntent().getExtras().getInt("trackId");
         String trackName = getIntent().getExtras().getString("trackName");
         String artistName = getIntent().getExtras().getString("artistName");
-        String lyricsBody = getIntent().getExtras().getString("lyrics");
-       // String artistName = getTitle(trackId);
+        String lyricsBody = getIntent().getExtras().getString("lyrics_body");
+        String lyricsCopy = getIntent().getExtras().getString("lyrics_copyright");
+        int lyricsId = getIntent().getExtras().getInt("lyrics_id");
+        String lyricsLang = getIntent().getExtras().getString("lyrics_language");
+        String pixel = getIntent().getExtras().getString("pixel_tracking_url");
+        String script = getIntent().getExtras().getString("script_tracking_url");
+       Lyrics lyrics =  makeLyrics(lyricsBody, lyricsCopy, lyricsId, lyricsLang, pixel, script);
+      TrackObject currTrack=  makeTrack(trackId, trackName, artistName, lyrics);
+        // String artistName = getTitle(trackId);
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle(artistName);
         actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#32353A")));
     }
 
+    public Lyrics makeLyrics(String lyricsBody, String lyricsCopy, int lyricsId, String lyricsLang, String pixel, String script) {
+        Lyrics lyrics = new Lyrics(lyricsBody, lyricsCopy, lyricsId, lyricsLang, pixel, script);
 
-    public TrackObject makeTrack(int trackId, String trackName,String artistName, String lyrics){
-        TrackObject currTrack = new TrackObject(trackId,trackName,artistName,lyrics);
+        return lyrics;
+    }
+
+    public TrackObject makeTrack(int trackId, String trackName, String artistName, String lyrics) {
+        TrackObject currTrack = new TrackObject(trackId, trackName, artistName, lyrics);
         return currTrack;
     }
 
